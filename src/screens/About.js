d7f25ad30168c756846464193d68ca81e219582c
cedric.brusson <cedric.brusson@gmail.com>
2021-04-13 08:48:37 +0200

    [UPDATE] About + Confidential

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { listeEmailAction } from '../redux/actions/listeEmailAction';
 import { pointingAction } from '../redux/actions/pointingHorsLigneAction';
-
+import * as Animatable from 'react-native-animatable';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 class About extends React.Component { 
 
     render(){
-        const text_about_1 = 'NIVA® version V1.0 2021';
-        const text_about_2 = 'Cette application a été réalisée par ALPHASYS - Lyon - France.';
-        const text_about_3 = 'NIVA est une marque déposée par ALPHASYS - Lyon - France.';
-        const text_about_4 = 'Cette application a pour objectif de : ';
-        const list_objectifs =  [
-            { key : 1, objectif: 'Récupérer les mouvements de début, fin ou changement d\'activité' },
-            { key : 2, objectif: 'Calculer et comptabiliser le temps écoulé entre deux mouvements' },
-            { key : 3, objectif: 'Informer l\'utilisateur sur le décompte d\'un certain nombre de compteurs affichés' },
-            { key : 4, objectif: 'Le calcul du temps écoulé ainsi que le choix des compteurs affichés sont de la responsabilité de votre employeur' },
+        const list_about =  [
+            { key : 1, text: 'NIVA® version V1.0 2021' },
+            { key : 2, text: 'Cette application a été réalisée par ALPHASYS - Lyon - France.' },
+            { key : 3, text: 'NIVA est une marque déposée par ALPHASYS - Lyon - France.' },
+            { key : 4, text: 'Cette application a pour objectif de : ' },
+            { key : 5, text: ' - Récupérer les mouvements de début, fin ou changement d\'activité' },
+            { key : 6, text: ' - Calculer et comptabiliser le temps écoulé entre deux mouvements' },
+            { key : 7, text: ' - Informer l\'utilisateur sur le décompte d\'un certain nombre de compteurs affichés' },
+            { key : 8, text: ' - Le calcul du temps écoulé ainsi que le choix des compteurs affichés sont de la responsabilité de votre employeur' },
         ];
         return(
             <View style = { styles.container }>
-                <Text style = { styles.text_about }>{ text_about_1 }</Text>
-                <Text style = { styles.text_about }>{ text_about_2 }</Text>
-                <Text style = { styles.text_about }>{ text_about_3 }</Text>
-                <Text style = { styles.text_about }>{ text_about_4 }</Text>
-                <FlatList data = { list_objectifs } renderItem = { ({ item }) => <Text style = { styles.text_about }>  -  { item.objectif }</Text>} />
+                <Animatable.View animation = "fadeInDown" style = { styles.container_header }>
+                    <View style = { styles.container_logo }>
+                        <View style = { styles.container_ico }>
+                            <Image style = { styles.image_top_body } source = { require ('../image/logo_niva.png') }/>
+                        </View>
+                    </View>
+                </Animatable.View>
+                <View style = { styles.container_body }>
+                    <Animatable.View animation = "bounceIn" style = { styles.text_body }>  
+                        <FlatList data = { list_about } renderItem = { ({ item }) => <Text style = { styles.text_about }>{ item.text }</Text>} />
+                    </Animatable.View>
+                </View>
             </View>
         );
     }
@@ -31,13 +39,43 @@ class About extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        paddingVertical: 10
+    container: {
+        flex: 1
     },
-    text_about : {
+    container_header: {
+        flex: 1,
+        padding: 10,
+        paddingTop: 20
+    },
+    container_body : {
+        flex : 2,
+        padding: 10
+    },
+    container_logo: {
+        flex: 1,
+        padding: 20, 
+        backgroundColor: '#008080',
+        elevation: 5,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    container_ico: {
+        backgroundColor: "#ECEFEC", 
+        borderRadius: 50, 
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text_about: {
         padding: 5
     },
+    text_body : {
+        padding: 30, 
+        backgroundColor: 'white',
+        elevation: 5,
+        borderRadius: 5,
+        marginBottom: 10
+    }, 
 });
 
 const mapStateToProps = (state) => {
--- src/screens/Confidential.js
+++ src/screens/Confidential.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { listeEmailAction } from '../redux/actions/listeEmailAction';
 import { pointingAction } from '../redux/actions/pointingHorsLigneAction';
+import * as Animatable from 'react-native-animatable';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 class Confidential extends React.Component { 
 
@@ -11,8 +13,21 @@ class Confidential extends React.Component {
         const text_confidential_2 = 'Les données échangées entre votre téléphone mobile et le serveur NIVA sont sécurisées et cryptées par certificat.';
         return(
             <View style = { styles.container }>
-                <Text style = { styles.text_confidential }>{ text_confidential_1 }</Text>
-                <Text style = { styles.text_confidential }>{ text_confidential_2 }</Text>
+                <Animatable.View animation = "fadeInDown" style = { styles.container_header }>
+                    <View style = { styles.container_logo }>
+                        <View style = { styles.container_ico }>
+                            <FontAwesome5 name = "user-lock" size = { 35 } color = "#008080" style = {{ padding:15 }}/>
+                        </View>
+                    </View>
+                </Animatable.View>
+                <View style = { styles.container_body }>
+                    <Animatable.View animation = "bounceIn">  
+                        <TouchableOpacity style = { styles.text_body }>
+                            <Text style = { styles.text_confidential }>{ text_confidential_1 }</Text>
+                            <Text style = { styles.text_confidential }>{ text_confidential_2 }</Text>
+                        </TouchableOpacity>
+                    </Animatable.View>
+                </View>
             </View>
         );
     }
@@ -20,13 +35,43 @@ class Confidential extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        paddingVertical: 10
+    container: {
+        flex: 1
     },
-    text_confidential : {
+    container_header: {
+        flex: 1,
+        padding: 10,
+        paddingTop: 20
+    },
+    container_body : {
+        flex : 2,
+        padding: 10
+    },
+    container_logo: {
+        flex: 1,
+        padding: 20, 
+        backgroundColor: '#008080',
+        elevation: 5,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    container_ico: {
+        backgroundColor: "#ECEFEC", 
+        borderRadius: 50, 
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text_confidential: {
         padding: 5
     },
+    text_body : {
+        padding: 30, 
+        backgroundColor: 'white',
+        elevation: 5,
+        borderRadius: 5,
+        marginBottom: 10
+    }, 
 });
 
 const mapStateToProps = (state) => {
